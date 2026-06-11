845612b5e6d1614468f97e0075c73929d51040cb
Manda mora <shiestybizz113@gmail.com>
2026-06-11 07:29:22 -0700

    Add landing page that connects to studio

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { LyricaPublicLanding } from "@/components/LyricaPublicLanding";
+import { useNavigate } from "react-router-dom";
+
+export default function LandingPage() {
+  const navigate = useNavigate();
+
+  const handleEnterStudio = () => {
+    navigate("/studio");
+  };
+
+  return <LyricaPublicLanding onEnterStudio={handleEnterStudio} />;
+}
